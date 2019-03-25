89d677dc6159584da637c8a1eed162e4df591f77
Yasmin Abdirahman <yasminosman37@yahoo.com>
2019-03-25 16:23:18 +0300

    form component created

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit,Output,EventEmmiter } from '@angular/core';
+import {Quote} from '../quote';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  @Output() emitQuote =new EventEmitter()
+  quoteString: string
+  quoteWriter:string
+  quoteAuthor:string
+  words:any
+
+  submitQuote(){
+    this.words= new Qoute("","","")
+    this.emitQuote.emit(this.words)
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
