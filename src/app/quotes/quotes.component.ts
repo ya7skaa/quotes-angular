5adeb72db6e24ec5d94c3c33dd0615d2c0280093
Yasmin Abdirahman <yasminosman37@yahoo.com>
2019-03-25 16:57:20 +0300

    functions of upvote and downvote written for quotes component

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{Quote} from '../quote';
 
 @Component({
   selector: 'app-quotes',
@@ -6,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+  quotes=[
+    new Quote("Yasmin", "Our greatest glory is not in never falling, but in rising every time we fall")
+    new Quote("Abdi.H","All our dreams can come true, if we have the courage to pursue them.")
+    new Quote("Sumaya","It does not matter how slowly you go as long as you do not stop.")
+  ]
+addQuote(pickedQuote){
+  this.quotes.push(pickedQuote)
+}
+upvote(i){
+  this.quotes[i].upvotes ++;
+}
+down.vote(i){
+  this.quotes[i].downvotes ++;
+}
+deleteQuote(i){
+  this.quotes.splice(i,1)
+}
 
   constructor() { }
 
